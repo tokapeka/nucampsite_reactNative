bdf2ffcf1e1e61c8d9a467df17b5fe8c0706ed3c
Anton Plesca <anton.plesca@yahoo.com>
2022-12-13 12:30:32 -0700

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,7 @@ import { ScrollView, Text, View } from 'react-native';
 import { Avatar, Card, ListItem } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponents';
 
 const Mission = () => {
     return (
@@ -22,6 +23,35 @@ const Mission = () => {
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
     
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>
+                        Community Partners
+                    </Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>
+                        Community Partners
+                    </Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+
     return (
         <ScrollView>
                 <Mission />
