3cddf5ed41e3d7f8d52608e4d245da326dd1769f
Anton Plesca <anton.plesca@yahoo.com>
2022-12-25 18:59:53 -0700

    gestures that trigger actions

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
+import { StyleSheet, Text, View, PanResponder, Alert, Modal } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable';
@@ -10,6 +10,7 @@ const RenderCampsite = (props) => {
     const view = useRef();
 
     const isLeftSwipe = ({ dx }) => dx < -200;
+    const isRightSwipe = ({ dx }) => dx > 200;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -43,6 +44,8 @@ const RenderCampsite = (props) => {
                     ],
                     { cancelable: false }
                 );
+            } else if (isRightSwipe(gestureState)) {
+                props.onShowModal()
             }
         }
     });
